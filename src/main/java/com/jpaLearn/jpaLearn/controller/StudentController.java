8a1d65d747044454d449ee617389183ea6a8e482
lslowik <test@email>
2025-02-09 17:54:56 +0100

    test2

---
+++ src/main/java/com/jpaLearn/jpaLearn/controller/StudentController.java
@@ -0,0 +1,17 @@
+package com.jpaLearn.jpaLearn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+    @GetMapping
+    public List<String> findAllStudents(){
+        return List.of("Lukasz", "Marek");
+    }
+}
