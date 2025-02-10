99f0df1c58a84f2bd37ec0976b9072730096271e
lslowik <test@email>
2025-02-10 07:42:15 +0100

    refactor StudentController to use StudentService for retrieving student data

---
+++ src/main/java/com/jpaLearn/jpaLearn/service/StudentService.java
@@ -0,0 +1,26 @@
+package com.jpaLearn.jpaLearn.service;
+
+import com.jpaLearn.jpaLearn.model.Student;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents() {
+    return List.of(
+            new Student("John", "Doe", 20, "john.doe@example.com",LocalDate.of(2003, 1, 1)),
+            new Student("Jane", "Smith", 22, "jane.smith@example.com", LocalDate.of(2001, 2, 2)),
+            new Student("Michael", "Johnson", 21, "michael.johnson@example.com", LocalDate.of(2002, 3, 3)),
+            new Student("Emily", "Davis", 23, "emily.davis@example.com", LocalDate.of(2000, 4, 4)),
+            new Student("David", "Wilson", 24, "david.wilson@example.com", LocalDate.of(1999, 5, 5)),
+            new Student("Sarah", "Brown", 25, "sarah.brown@example.com", LocalDate.of(1998, 6, 6)),
+            new Student("Chris", "Jones", 26, "chris.jones@example.com", LocalDate.of(1997, 7, 7)),
+            new Student("Jessica", "Garcia", 27, "jessica.garcia@example.com", LocalDate.of(1996, 8, 8)),
+            new Student("Daniel", "Martinez", 28, "daniel.martinez@example.com", LocalDate.of(1995, 9, 9)),
+            new Student("Laura", "Hernandez", 29, "laura.hernandez@example.com", LocalDate.of(1994, 10, 10))
+
+            );
+}
+}
